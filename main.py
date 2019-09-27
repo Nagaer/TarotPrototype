c9af6d762edb5957788afff9e9e4cddafc1b6ebc
Nagear <faraon842@gmail.com>
2019-09-27 23:34:22 +0300

    add concole args

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
+import click
 from pygame.constants import QUIT
 
 WIDTH = 1024
 HEIGHT = 768
 TITLE = 'TarotPrototype'
+FULSCREEN = False
 
 
-def main():
+@click.command()
+@click.option('-s', '--size', default=(WIDTH, HEIGHT))
+@click.option('-t', '--title', default=TITLE)
+@click.option('--fullscreen', is_flag=True, default=FULSCREEN)
+def main(size, title, fullscreen):
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption(TITLE)
+    if fullscreen:
+        screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+    else:
+        screen = pygame.display.set_mode(size)
+    pygame.display.set_caption(title)
+    background = pygame.image.load('image.png')
+    background_rect = background.get_rect(bottomright=size)
+    screen.blit(background, background_rect)
+    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
